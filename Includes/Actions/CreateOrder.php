db21dc96235cb03e6f5ad69d3778cde32a19b82e
Darkcdm <darksniped@gmail.com>
2022-03-07 22:13:10 +0100

    Set the algoryth for checking used workers

--- Includes/Actions/CreateOrder.php
+++ Includes/Actions/CreateOrder.php
@@ -12,6 +12,7 @@ $Resource = $_GET["Resource"];
 $ResourceAmount = $_GET["Amount"];
 
 
+$backE->CheckWorkers($AssignWorkers);
 //get data about the ordered resource 
 $Extract = $db->GetData('SELECT * FROM DirtGame.Resource Where ResourceName = "' . $Resource . '";');
 
@@ -46,8 +47,40 @@ echo "<script>window.close();</script>";
 
 class OrderBackEnd
 {
-
     //functions to clean main code
+    public function CheckWorkers($AssignWorkers)
+    {
+        $db = new dbTool();
+
+        //get amount of free workers
+        //get amount of workers being used + get amount of workers one player has
+        $sql = "SELECT PeopleAmount
+        FROM Users_Population
+        WHERE PeopleID = 1;";
+
+        $data = $db->GetData($sql);
+        $MaxAmountOfWorkers = $data["PeopleAmount"];
+
+        $sql = "SELECT COUNT(UsedWorkers)
+        FROM Orders
+        WHERE idUser = " . $_SESSION["UserID"] . ";";
+
+        $data = $db->GetData($sql);
+        $AmountOfWorkersInUse = $data["COUNT(UsedWorkers)"];
+
+        if ($AmountOfWorkersInUse < $MaxAmountOfWorkers) {
+            //user have workers in reserve
+            $workersToBeUsed = $MaxAmountOfWorkers - $AmountOfWorkersInUse - $AssignWorkers;
+
+            if ($workersToBeUsed <= 0) {
+                //user doesn't have enough workers left for his order
+                $this->Alert($workersToBeUsed);
+            }
+        } else {
+            //user has no workers left
+
+        }
+    }
     public function mining($resourceID, $ResourceAmount, $AssignWorkers, $resouceWorkTime)
     {
         $db = new dbTool();
@@ -56,15 +89,12 @@ class OrderBackEnd
         //put order into database
         $sql =
             "INSERT INTO `DirtGame`.`Orders` (`idUser`, `type`, `ResourceID`, `Amount`, `startingTime`, `OrderTime`, `UsedWorkers`) 
-    VALUES ('" . $_SESSION["UserID"] . "', 'mine', '" . $resourceID . "', '" . $ResourceAmount . "', CURRENT_TIMESTAMP(), SEC_TO_TIME(" . $workTime . "), " . $AssignWorkers . ");";
+        VALUES ('" . $_SESSION["UserID"] . "', 'mine', '" . $resourceID . "', '" . $ResourceAmount . "', CURRENT_TIMESTAMP(), SEC_TO_TIME(" . $workTime . "), " . $AssignWorkers . ");";
 
         $db->SetData($sql);
 
         echo '<script> alert("Order Created!");</script>';
     }
-
-
-
     public function crafting($resourceID, $ResourceAmount, $AssignWorkers)
     {
         $db = new dbTool();
@@ -170,5 +200,9 @@ class OrderBackEnd
         $resourceName = $db->GetData($sql);
         $alertString = "you're missing " . $ResourceAmount  . " of " . $resourceName["ResourceName"];
         echo '<script> alert("' . $alertString . '");</script>';
+    }
+    private function Alert($msg)
+    {
+        echo '<script> alert("' . $msg . '");</script>';
     }
 }
